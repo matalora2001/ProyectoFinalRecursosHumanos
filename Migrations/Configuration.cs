3731c2bee6e909f0fea4a949f6533ab3f9111a60
MataLora2001 <abbymatalora@gmail.com>
2019-12-03 22:16:19 -0500

    + Vista de Departamentos + carpeta para migraciones en la DB

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+﻿namespace ProyectoFinal.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ProyectoFinalRecursosHumanos.Context.GestionContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(ProyectoFinalRecursosHumanos.Context.GestionContext context)
+        {
+
+        }
+    }
+}
